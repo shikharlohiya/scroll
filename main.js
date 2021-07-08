7b853da77e8553e5ddafc2372af5de9839a7bedd
shikharlohiya <anshullohiya321@gmail.com>
2021-07-08 22:29:34 +0530

    final changes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var tl = gsap.timeline({
   }
 });
 
-function display_fun(element , class1 , class2 , class3 , class4 ,class5,y3,y4,stroke ) {
+function display_fun(element , class1 , class2 , class3 , class4 ,class5,y3,y4,stroke,class6, class7, class8) {
   console.log(element);
   console.log(stroke)
   tl.from(class1, {
@@ -33,6 +33,27 @@ function display_fun(element , class1 , class2 , class3 , class4 ,class5,y3,y4,s
     duration: 1,
     y: y4  
   });
+  
+ gsap.from(class6,{
+   opacity: 0,
+   duration: 1,
+  y:y4
+ });
+ gsap.from(class7,{
+  opacity: 0,
+  duration: 1,
+  y :y4
+
+});
+gsap.from(class8,{
+  opacity: 0,
+  duration: 1,
+  y :y3
+
+});
+
+
+
   var i;
   for(i=1;i<=7;i++){
     if(element!=i){
@@ -89,7 +110,7 @@ var tl3 = gsap.timeline({
     start: "center 56%",
     end: "center 43%",
     onEnterBack: () => display_fun('2','.col1-left-part-3','.left-sub-head-3','#Opaque_Ring','.img-1','.img-2','200','-500','420'),
-    onLeave: () => display_fun('3','.c3-rp-img-1','.c3-rp-img-2', '#Opaque_Ring','.c3-rp-img-3','.c3-rp-img-4','.c3-rp-img-5','200','560')
+    onLeave: () => display_fun('3','.col1-left-part-4', 'left-sub-head-5', '#Opaque_Ring','.c3-rp-img-3','.c3-rp-img-1','-300','200','560','.c3-rp-img-2' , '.c3-rp-img-4' , '.c3-rp-img-5')
   }
 });
 
@@ -100,7 +121,7 @@ var tl4 = gsap.timeline({
     start: "center 43%",
     end: "center 30%",
     
-    onEnterBack: () => display_fun('3','.c3-rp-img-1','.c3-rp-img-2', '#Opaque_Ring','.c3-rp-img-3','.c3-rp-img-4','.c3-rp-img-5','200','560'),
+    onEnterBack: () => display_fun('3','.col1-left-part-4', 'left-sub-head-5', '#Opaque_Ring','.c3-rp-img-3','.c3-rp-img-1','-300','200','560','.c3-rp-img-2' , '.c3-rp-img-4' , '.c3-rp-img-5'),
     onLeave: () => display_fun('4','.col1-left-part-2','.left-sub-head-7', '#Opaque_Ring' ,'.bottom-img','.c4-rp-img','200','700','700')
   }
 });
@@ -112,7 +133,7 @@ var tl5 = gsap.timeline({
     start: "center 30%",
     end: "center 15%",
     onEnterBack: () => display_fun('4','.col1-left-part-2','.left-sub-head-7', '#Opaque_Ring' ,'.bottom-img','.c4-rp-img','200','700','700'),
-    onLeave: () => display_fun('5', '.col1-left-part-2','.left-sub-head-3','#Opaque_Ring','.bottom-img','.c5-rp-img1','100','-200','840')
+    onLeave: () => display_fun('5', '.col1-left-part-2','.left-sub-head-3','#Opaque_Ring','.c5-rp-img2','.c5-rp-img1','100','-200','840')
   }
 });
 
@@ -121,7 +142,7 @@ var tl6 = gsap.timeline({
     trigger: '.big-div',
     start: "center 15%",
     end: "center -2%",
-    onEnterBack: () => display_fun('5', '.col1-left-part-6','.left-sub-head-3','#Opaque_Ring','.bottom-img','.c5-rp-img1','100','-200','840'),
+    onEnterBack: () => display_fun('5', '.col1-left-part-6','.left-sub-head-3','#Opaque_Ring','.c5-rp-img2','.c5-rp-img1','100','-200','840'),
     onLeave: () => display_fun('6', '.col1-left-part-0' , '.nh-c2-1','#Opaque_Ring' ,'.c6-rp img' ,'.bottom-img','700','100','10 1000')
   }
 });
